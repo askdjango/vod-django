b8afc00a1408073c20b75e167f0770f491b34e96
Chinseok Lee <allieuslee@gmail.com>
2017-02-27 16:10:29 +0900

    첫 앱, URLConf, 뷰, 템플릿

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+# blog/urls.py
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html')
