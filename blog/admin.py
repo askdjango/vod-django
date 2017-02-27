55ab5a0d0cb664487fdc23eef397767cca65f6e1
Chinseok Lee <allieuslee@gmail.com>
2017-02-27 22:41:30 +0900

    blog.models.Post 모델 생성/마이그레이션/admin등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
+# blog/admin.py
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+# blog/models.py
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='제목',
+        help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')  # 길이 제한이 있는 문자열
+    content = models.TextField(verbose_name='내용')              # 길이 제한이 없는 문자열
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
