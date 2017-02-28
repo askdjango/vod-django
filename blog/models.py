b4c1bf43b9343b51c0c78726c4024f5870b7cfd1
Chinseok Lee <allieuslee@gmail.com>
2017-02-28 14:11:32 +0900

    blog.models.Post 모델에 기본정렬 지정

--- blog/models.py
+++ blog/models.py
@@ -28,5 +28,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-id']
+
     def __str__(self):
         return self.title
