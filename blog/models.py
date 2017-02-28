229b71d04a189e069e435355dd8e605c7c1ac388
Chinseok Lee <allieuslee@gmail.com>
2017-02-28 09:45:45 +0900

    blog.models.Post 모델에 필수필드인 author 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목',
         help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')  # 길이 제한이 있는 문자열
     content = models.TextField(verbose_name='내용')              # 길이 제한이 없는 문자열
