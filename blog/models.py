82df5f43ed15278fb896f6059e1512ced241f1ba
Chinseok Lee <allieuslee@gmail.com>
2017-02-28 16:27:46 +0900

    blog.models.Post 모델에 user 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 # blog/models.py
 import re
+from django.conf import settings
 from django.db import models
 from django.forms import ValidationError
 
@@ -16,7 +17,7 @@ class Post(models.Model):
         ('w', 'Withdrawn'),
     )
 
-    author = models.CharField(max_length=20)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=100, verbose_name='제목',
         help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')  # 길이 제한이 있는 문자열
     content = models.TextField(verbose_name='내용')              # 길이 제한이 없는 문자열
