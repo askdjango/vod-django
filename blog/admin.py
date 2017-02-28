2ef5bfe1496771b253e47f6c7e2def18f391b45f
Chinseok Lee <allieuslee@gmail.com>
2017-02-28 12:18:08 +0900

    blog.models.Post 모델에 대한 PostAdmin 커스텀

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+
+# admin.site.register(Post, PostAdmin)
