5d3441d7696eb68d8e8d995a369212a253b89a71
Chinseok Lee <allieuslee@gmail.com>
2017-02-28 16:30:12 +0900

    shop 앱 생성/등록

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'blog',
     'dojo',
+    'shop',
 ]
 
 MIDDLEWARE = [
--- askdjango/urls.py
+++ askdjango/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^dojo/', include('dojo.urls')),
+    url(r'^shop/', include('shop.urls')),
 ]
 
 if settings.DEBUG:
