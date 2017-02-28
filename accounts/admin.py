e611680ffe793d8c2eb7f748a0970e49d1379699
Chinseok Lee <allieuslee@gmail.com>
2017-02-28 16:09:18 +0900

    accounts.models.Profile 모델 생성/admin 등록

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
+# accounts/admin.py
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+# accounts/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)  # FIXME: BAD CASE !!!
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
