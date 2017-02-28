73299dec4ec19e2761086cf05b8c003980b55aa3
Chinseok Lee <allieuslee@gmail.com>
2017-02-28 15:28:11 +0900

    blog.views.post_detail 뷰를 구현하고, 올바른 404 처리

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^(?P<id>\d+)/$', views.post_detail),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 # blog/views.py
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
@@ -13,4 +14,17 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
         'q': q,
+    })
+
+
+def post_detail(request, id):
+#   try:
+#       post = Post.objects.get(id=id)
+#   except Post.DoesNotExist:
+#       raise Http404
+    
+    post = get_object_or_404(Post, id=id)
+
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
     })
