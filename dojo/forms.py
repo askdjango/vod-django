c87ce8589984c25a19e847895aca8196a340dce0
Chinseok Lee <allieuslee@gmail.com>
2017-03-10 20:03:24 +0900

    dojo.models.Post 모델에 user_agent 필드 추가

--- dojo/forms.py
+++ dojo/forms.py
@@ -7,7 +7,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         # fields = '__all__'
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'user_agent']
+        widgets = {
+            'user_agent': forms.HiddenInput,
+        }
 
     '''
     def save(self, commit=True):
--- dojo/models.py
+++ dojo/models.py
@@ -12,6 +12,7 @@ def min_length_3_validator(value):
 class Post(models.Model):
     title = models.CharField(max_length=100, validators=[min_length_3_validator])
     content = models.TextField()
+    user_agent = models.CharField(max_length=200)
     ip = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
