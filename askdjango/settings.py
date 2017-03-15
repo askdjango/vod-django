de7b671974cb7abd0fd4fbfd273363c60b8ab1cd
Chinseok Lee <allieuslee@gmail.com>
2017-03-15 10:53:59 +0900

    settings.STATIC_ROOT 설정을 추가하고, collectstatic 명령 실습

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'askdjango', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 INTERNAL_IPS = ['127.0.0.1']
