23c82b6ed3e01e6c4b5e8ef5486f56a00f4badb5
Chinseok Lee <allieuslee@gmail.com>
2017-03-15 11:11:35 +0900

    MEDIA 세팅을 하고, blog.models.Post 모델에 photo 필드 추가

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -133,6 +133,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 INTERNAL_IPS = ['127.0.0.1']
 
--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목',
         help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')  # 길이 제한이 있는 문자열
     content = models.TextField(verbose_name='내용')              # 길이 제한이 없는 문자열
+    photo = models.ImageField(blank=True)
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True,
         validators=[lnglat_validator],
