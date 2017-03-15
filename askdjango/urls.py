640bf50a8211497cb1b9e38b1068ee1e4e49f94b
Chinseok Lee <allieuslee@gmail.com>
2017-03-15 11:14:05 +0900

    media 파일 서빙 지원

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.views.generic import RedirectView
@@ -34,6 +35,8 @@ urlpatterns = [
     url(r'^dojo/', include('dojo.urls', namespace='dojo')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
