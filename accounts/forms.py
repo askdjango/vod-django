c2f1b87653d46c0253dc969b5783aeed9b652a32
Chinseok Lee <allieuslee@gmail.com>
2017-03-20 15:23:39 +0900

    회원가입 시에 휴대폰번호/주소 추가 입력

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,20 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class SignupForm(UserCreationForm):
+    phone_number = forms.CharField()
+    address = forms.CharField()
+
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
+
+    def save(self):
+        user = super().save()
+        profile = Profile.objects.create(
+            user = user,
+            phone_number = self.cleaned_data['phone_number'],
+            address = self.cleaned_data['address'])
+        return user
 
