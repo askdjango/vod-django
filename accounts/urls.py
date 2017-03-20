44078f00c59a14ba02cfeffd8c65e020d2c6d33f
Chinseok Lee <allieuslee@gmail.com>
2017-03-20 14:54:02 +0900

    회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^profile/$', views.profile),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,20 @@
 # accounts/views.py
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL)  # default : "/accounts/login/"
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
 
 
 def profile(request):
