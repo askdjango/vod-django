cc392457771c406f7be2a217f423296775efac09
Chinseok Lee <allieuslee@gmail.com>
2017-03-20 15:19:11 +0900

    회원가입 시에 이메일 추가 입력

---
+++ accounts/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,18 @@
 # accounts/views.py
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
+from .forms import SignupForm
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect(settings.LOGIN_URL)  # default : "/accounts/login/"
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup_form.html', {
         'form': form,
     })
