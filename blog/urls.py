ebcf9793c49170ae338a0881ebabecf4b223c59e
Chinseok Lee <allieuslee@gmail.com>
2017-03-20 11:17:44 +0900

    CBV : DetailView 적용

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views_cbv
 
 urlpatterns = [
     url(r'^$', views_cbv.post_list, name='post_list'),
-    url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^(?P<pk>\d+)/$', views_cbv.post_detail, name='post_detail'),
 
     url(r'^new/$', views.post_new, name='post_new'),
     url(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,9 +1,12 @@
 from django import forms
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
 post_list = ListView.as_view(model=Post, paginate_by=10)
+
+
+post_detail = DetailView.as_view(model=Post)
 
 
 # blog/forms.py
